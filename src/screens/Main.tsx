d7511a430e93983f6f27d1408f567a5d5c2f48fe
EvilGriffith <nevergvup22@gmail.com>
2024-06-07 22:08:03 +0300

    sd

---
+++ src/screens/Main.tsx
@@ -0,0 +1,15 @@
+import "../css/Main.css"
+
+export const Main = () => {
+    return(
+        <div className="window">
+            <div className="table">
+                <div className="logo"/>
+                <div className="but" id="firstbut">Game</div>
+                <div className="but">Settings</div>
+                <div className="cards"/>
+                
+            </div>
+        </div>
+    )
+}
